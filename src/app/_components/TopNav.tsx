2c4252242e4cff8d6e8448809b2903c6ae063075
Jabril Mahamud (personal account) <jabrilmahamud8@gmail.com>
2024-12-27 14:41:28 +0000

    setup shadcn UI

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,27 +1,25 @@
-"use client";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+import { SimpleUploadButton } from "./custom-upload-button-start";
 
 export function TopNav() {
-  const router = useRouter();
   
 
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div className="font-bold">Gallery</div>
 
-      <div className="flex flex-row">
+      <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton />
         </SignedOut>{" "}
         <SignedIn>
-          <UploadButton
+          {/* <UploadButton
             endpoint="imageUploader"
             onClientUploadComplete={() => {
               router.refresh();
             }}
-          />
+          /> */}
+          <SimpleUploadButton />
           <UserButton />
         </SignedIn>
       </div>
