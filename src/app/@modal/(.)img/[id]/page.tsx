896caabf867a750c71f84fcffbfee3d3b5260606
Jabril Mahamud (personal account) <jabrilmahamud8@gmail.com>
2024-12-27 11:37:52 +0000

    more modal cleanup

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { getImage } from "~/server/queries";
 import { Modal } from "./modal";
-import FullPageImageView from "~/server/full-image-page";
+import FullPageImageView from "~/components/full-image-page";
 
 export default function PhotoModal({
   params: { id: photoId },
--- src/server/full-image-page.tsx
+++
@@ -1,6 +0,0 @@
-import { getImage } from "./queries";
-
-export default async function FullPageImageView(props: { id: number }) {
-  const image = await getImage(props.id);
-  return <img src={image.url} className="w-96" />;
-}
