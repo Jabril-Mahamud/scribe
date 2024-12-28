d0cdf9236b195147f74dc12c9aedcc545caa92e3
Jabril Mahamud (personal account) <jabrilmahamud8@gmail.com>
2024-12-28 17:23:51 +0000

    analytics on delete button

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -5,6 +5,7 @@ import { images } from "./db/schema";
 import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import analyticsServerClient from "./analytics";
 
 export async function getMyImages() {
   const user = await auth();
@@ -53,6 +54,16 @@ export async function deleteImage(id: number) {
   await db
     .delete(images)
     .where(and(eq(images.id, id), eq(images.userId, user.userId)));
+
+
+    analyticsServerClient.capture({
+      distinctId: user.userId,
+      event: "delete image 🗑️",
+      properties: {
+        imageId: id,
+      },
+    });
+    
   
     // revalidatePath("/")
     redirect("/")
